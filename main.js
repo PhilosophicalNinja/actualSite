4ae129ca9a0c20fdfe6b127810243f63553ff6fd
Philosophical Ninja <www.092334@gmail.com>
2023-11-06 07:57:38 -0600

    Prototype

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 var canvas = document.querySelector("canvas");
+var resetBtn = document.getElementById("resetBtn");
+var selectMenu = document.getElementById("selectMenu");
 var ctx = canvas.getContext("2d");
 canvas.addEventListener("click", input);
+resetBtn.addEventListener("click", initialize);
+selectMenu.addEventListener("change", promote);
 
 //var black = "#1204d6";
 //var white = "aqua";
@@ -40,8 +44,11 @@ pieces.w_king.alt = "w_king";
 pieces.w_knight.src = "w_knight_png_256px.png";
 pieces.w_knight.alt = "w_knight";
 pieces.w_pawn.src = "w_pawn_png_256px.png";
+pieces.w_pawn.alt = "w_pawn";
 pieces.w_queen.src = "w_queen_png_256px.png";
+pieces.w_queen.alt = "w_queen";
 pieces.w_rook.src = "w_rook_png_256px.png";
+pieces.w_rook.alt = "w_rook";
 
 
 class Square {
@@ -75,28 +82,36 @@ for(let i = 0; i < 8; i++){
     }
 }
 
-board[0][0].piece = pieces.b_rook;
-board[1][0].piece = pieces.b_knight;
-board[2][0].piece = pieces.b_bishop;
-board[3][0].piece = pieces.b_queen;
-board[4][0].piece = pieces.b_king;
-board[5][0].piece = pieces.b_bishop;
-board[6][0].piece = pieces.b_knight;
-board[7][0].piece = pieces.b_rook;
-for(let i = 0; i < 8; i++){
-    board[i][1].piece = pieces.b_pawn;
-}
+function initialize(){
+    for(let i = 0; i < 8; i++){
+        for(let j = 0; j < 8; j++){
+            board[i][j].piece = "empty"; 
+        }
+    }
+    board[0][0].piece = pieces.b_rook;
+    board[1][0].piece = pieces.b_knight;
+    board[2][0].piece = pieces.b_bishop;
+    board[3][0].piece = pieces.b_queen;
+    board[4][0].piece = pieces.b_king;
+    board[5][0].piece = pieces.b_bishop;
+    board[6][0].piece = pieces.b_knight;
+    board[7][0].piece = pieces.b_rook;
+    for(let i = 0; i < 8; i++){
+        board[i][1].piece = pieces.b_pawn;
+    }
 
-board[0][7].piece = pieces.w_rook;
-board[1][7].piece = pieces.w_knight;
-board[2][7].piece = pieces.w_bishop;
-board[3][7].piece = pieces.w_queen;
-board[4][7].piece = pieces.w_king;
-board[5][7].piece = pieces.w_bishop;
-board[6][7].piece = pieces.w_knight;
-board[7][7].piece = pieces.w_rook;
-for(let i = 0; i < 8; i++){
-    board[i][6].piece = pieces.w_pawn;
+    board[0][7].piece = pieces.w_rook;
+    board[1][7].piece = pieces.w_knight;
+    board[2][7].piece = pieces.w_bishop;
+    board[3][7].piece = pieces.w_queen;
+    board[4][7].piece = pieces.w_king;
+    board[5][7].piece = pieces.w_bishop;
+    board[6][7].piece = pieces.w_knight;
+    board[7][7].piece = pieces.w_rook;
+    for(let i = 0; i < 8; i++){
+        board[i][6].piece = pieces.w_pawn;
+    }
+    render();
 }
 
 function render(cellx = null, celly = null){
@@ -127,29 +142,179 @@ function render(cellx = null, celly = null){
     }
 }
 
+
+function promote(){
+    switch(selectMenu.value){
+        case "queen":
+            board[inputObj.x2][inputObj.y2].piece = pieces.w_queen;
+            break;
+        case "knight":
+            board[inputObj.x2][inputObj.y2].piece = pieces.w_knight;
+            break;
+        case "rook":
+            board[inputObj.x2][inputObj.y2].piece = pieces.w_rook;
+            break;
+        case "bishop":
+            board[inputObj.x2][inputObj.y2].piece = pieces.w_bishop;
+            break;
+        default:
+            console.log("Something has gone wrong.");
+            break;
+    }
+        render(inputObj.x2, inputObj.y2);
+        selectMenu.style.visibility = "hidden";
+        selectMenu.value = "select";
+}
+
+function verifyMove(){
+    let diffX = Math.abs(inputObj.x1 - inputObj.x2);
+    let directX = Math.pow(inputObj.x2 - inputObj.x1, 0);
+    let diffY = Math.abs(inputObj.y1 - inputObj.y2);
+    let directY = Math.pow(inputObj.y2 - inputObj.y1, 0);
+    let pass = true;
+    let occupied = false;
+    //phase one: check if movement pattern is valid
+    switch(board[inputObj.x1][inputObj.y1].piece.alt.slice(2)){
+        case "rook": 
+            if(!((diffX > 0 && diffY == 0) || (diffX == 0 && diffY > 0))){
+                pass = false;
+            }
+            break;
+        case "bishop":
+            if(!(diffX == diffY)){
+                pass = false;
+            }
+            break;
+        case "knight":
+            if(!((diffX == 2 && diffY == 1) || (diffX == 1 && diffY == 2))){
+                pass = false;
+            }
+            break;
+        case "queen":
+            if((diffX > 0 && diffY == 0) || (diffX == 0 && diffY > 0)){
+                break;
+            } else if(diffX == diffY){
+                break;
+            } else {
+                pass = false;
+            }
+            break;
+        case "pawn":
+            if(diffY == 1 && diffX == 0 && board[inputObj.x2][inputObj.y2].piece == "empty"){
+                break;
+            } else if((diffX == diffY && diffY == 1) && board[inputObj.x2][inputObj.y2].piece != "empty"){
+                break;
+            } else {
+                pass = false;
+            }
+            break;
+        case "king":
+            if(diffY > 1 || diffX > 1){
+                pass = false;
+            }
+            break;
+        default:
+            console.error("Unidentified piece movement");
+
+    }
+    //phase two: check if piece blocks the shot
+    switch(board[inputObj.x1][inputObj.y1].piece.alt.slice(2)){
+        case "rook":
+            if(diffX > 1){
+                console.log(diffX)
+                if(directX > 0){
+                    for(let i = inputObj.x1 + 1; i < inputObj.x2; i++){
+                        if(board[i][inputObj.y1].piece != "empty"){
+                            if(occupied){
+                                pass = false;
+                            } else {
+                                occupied = true;
+                            }
+                        }
+                    }
+                } else if(directX < 0){
+                    for(let i = inputObj.x1 - 1; i < inputObj.x2; i--){
+                        if(board[i][inputObj.y1].piece != "empty"){
+                            if(occupied){
+                                pass = false;
+                            } else {
+                                occupied = true;
+                            }
+                        }
+                    }
+                }
+            }
+    //phase three: if attacking piece, check for enemy
+        }
+    return pass;
+        
+}
+
 function move(){
-    
+    let isMoveLegal = verifyMove();
+    if(!(inputObj.x1 == inputObj.x2 && inputObj.y1 == inputObj.y2) && isMoveLegal){
+        let change = false;
+        if(board[inputObj.x1][inputObj.y1].piece.alt.slice(2) == "pawn" && inputObj.y2 == 0){
+            change = true;
+        }
+        board[inputObj.x2][inputObj.y2].piece = board[inputObj.x1][inputObj.y1].piece;
+        board[inputObj.x1][inputObj.y1].piece = "empty";
+        board[inputObj.x1][inputObj.y1].color = inputObj.firstColor;
+        board[inputObj.x2][inputObj.y2].color = inputObj.secondColor;
+        render(inputObj.x1, inputObj.y1);
+        render(inputObj.x2, inputObj.y2);
+        if(change){
+            selectMenu.style.visibility = "visible";
+            selectMenu.style.top = inputObj.absy;
+            selectMenu.style.left = inputObj.absx;
+        }
+        inputObj.first = false;
+    } else {
+        board[inputObj.x1][inputObj.y1].color = inputObj.firstColor;
+        board[inputObj.x2][inputObj.y2].color = inputObj.secondColor;
+        render(inputObj.x1, inputObj.y1);
+        render(inputObj.x2, inputObj.y2);
+        inputObj.first = false;
+    }
 }
 
 var inputObj = {
+    absx: 0,
+    absy: 0,
     x1: 0,
     y1: 0,
+    firstColor: null,
     first: false,
     x2: 0,
-    y2: 0
+    y2: 0,
+    secondColor: null
 }
 
 function input(e){
     console.log("__________________________________________________________");
+    inputObj.absx = e.clientX;
+    inputObj.absy = e.pageY;
     let x = Math.floor(e.offsetX/50);
     let y = Math.floor(e.offsetY/50);  
-    board[x][y].color = "yellow";
-    render(x, y);
+    if(inputObj.first == false){
+        if(board[x][y].piece != "empty"){
+            inputObj.firstColor = board[x][y].color;
+            board[x][y].color = "yellow";
+            inputObj.x1 = x;
+            inputObj.y1 = y;
+            render(x, y);
+            inputObj.first = true;
+        }
+    } else {
+        inputObj.secondColor = board[x][y].color;
+        board[x][y].color = "red";
+        inputObj.x2 = x;
+        inputObj.y2 = y;
+        render(x, y);
+        move();
+    }
+    
 }
 
-setTimeout(render, 1000);
-
-
-//canvas.addEventListener("onload", ()=>ctx.drawImage(list[0], 0, 0, 50, 50));
-//setTimeout(()=>ctx.drawImage(pieces.w_king, 0, 0, 50, 50), 1000);
+window.onload = initialize;
 var a = []; a['cat']='meow';
---
+++ red words.md
@@ -0,0 +1,50 @@
+# Author:
+1. Nathanial Hawthorne
+2. Descended from Judge Hathorne
+
+# Times:
+1. Written in 1850 (Customs house happened then)
+2. Chapter one happens: Boston 1642
+
+Wrote about Hester Prynne (Protagonist)
+
+Whigs kick the Democrats out, so bye bye narrator.
+
+# Wrote about
+1. Family history
+2. History of the Locale
+
+# Romantic Author - Romanticism
+1. Exaggerated stuff
+
+
+# Chapter One:
+1. Boston 1692 in the month of June
+2. Sprung up from sainthood Anne Hutchison (First Feminist) Death by decapitation via Indian. Massachusets bay colony.
+
+# Chapter Two:
+
+1. Hester Prynee exits the prison with her 3-month old child, and mounts the scaffhold.
+2. Tried for Adultery.
+3. Hester was sent early to set up a home. Husband delayed by two years. She has a child out of wedlock.
+4. Brown hair. Looks like the Virgin Mary.
+5. Thinks of her time in England, and of her husband. A boring dude, way older than her, with a shoulder much higher than the other. 
+
+# Chapter Three:
+1. She goes from watching her memories to seeing her husband in the outskirts of the crowd. He was captured by Indians for two years.
+2. He makes a gesture at her. A shushing gesture.
+3. He learns about his wife's crime through a stranger.
+ # Presiding over the thing.
+4. Reverend John Wilson (Sermon about sin)
+5. Governer Bellingham
+6. Reverend Dimmsdale (Young one with good personality)
+7. Give the name of the father. "No"
+8. Daughter's name is Pearl. Only good thing to give.
+
+
+Ignominy
+Physiognomy
+Edifice
+Magistrate
+Iniquity
+Sojourn
